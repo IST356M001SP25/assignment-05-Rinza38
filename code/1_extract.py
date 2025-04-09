50221ed84cb78904f41d9f85770837b86af50b71
Philip Cheuk <Pcheuk@syr.edu>
2025-04-09 14:56:03 -0400

    Changes

--- code/1_extract.py
+++ code/1_extract.py
@@ -18,7 +18,7 @@ years = survey['year'].unique()
 for year in years:
     col_year = pd.read_html(f"https://www.numbeo.com/cost-of-living/rankings.jsp?title={year}&displayColumn=0")
     col_year = col_year[1]
-    col_year['year'] = yearcol_year.to_csv(f'cache/col_{ear}.csv', index=False)   
+    col_year['year'] = col_year.to_csv(f'cache/col_{year}.csv', index=False)   
 
 states = pd.read_csv("https://docs.google.com/spreadsheets/d/14wvnQygIX1eCVo7H5B7a96W1v5VCg6Q9yeRoESF6epw/export?format=csv")
 states.to_csv('cache/states.csv', index=False)
